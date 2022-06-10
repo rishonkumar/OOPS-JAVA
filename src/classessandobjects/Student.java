d0299b7edb5b0e2edfe07880213e9b175772b5c8
rishonkumar <rishonkumarrahi@gmail.com>
2022-06-10 09:46:33 +0530

    OOPS

---
+++ src/classessandobjects/Student.java
@@ -0,0 +1,8 @@
+package classessandobjects;
+
+public class Student {
+
+//    Student properties
+    String name;
+    int rollNumber;
+}
---
+++ src/classessandobjects/StudentUse.java
@@ -0,0 +1,12 @@
+package classessandobjects;
+
+import java.util.Scanner;
+
+public class StudentUse {
+
+    public static void main(String[] args) {
+
+//        to create object of Student
+        Student s1 = new Student();
+    }
+}
